39a000a9916676230321f047da5a26bbc151510c
Pafnow <pafnow@hotmail.com>
2020-08-14 12:35:18 +0700

    Squashed 'product_weight_uom/' content from commit 3c44e05
    git-subtree-dir: product_weight_uom
    git-subtree-split: 3c44e05ed2f6205008cede1cd975fbd156065693

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Pafnow
+{
+    'name': 'Product Weight UOM',
+    'version': '13.0.1.0.0',
+    'depends': ['product','uom'],
+    'author': 'Pafnow',
+    'category': 'Product',
+    'description': 'Calculate product weight based on selected UOM',
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
